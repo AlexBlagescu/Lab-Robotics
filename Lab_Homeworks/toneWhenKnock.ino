a5a562b1c0121a6d59a1b4871d1a1ce2fd266a1b
AlexBlagescu <44620859+AlexBlagescu@users.noreply.github.com>
2019-11-11 20:21:44 +0200

    Create toneWhenKnock.ino

---
+++ Lab_Homeworks/toneWhenKnock.ino
@@ -0,0 +1,34 @@
+const int knockSensor = A0; 
+const int pushButton = 2;
+const int buzzerPin = 8;
+const int toneTime = 5000;
+const int toneFrequency = 1000;
+
+int sensorReading = 0;     
+int buttonState = 0;
+int timeSensorDetectKnock = -1;
+int currentTime = 0;
+int buttonValue = 0;
+
+void setup() {
+  Serial.begin(9600);       
+  pinMode(pushButton, INPUT_PULLUP);
+}
+
+void loop() {
+  currentTime = millis();
+  sensorReading = analogRead(knockSensor);
+  buttonState = digitalRead(pushButton);
+  buttonValue = !buttonState;
+  if (sensorReading > 0 && timeSensorDetectKnock == -1) {
+    timeSensorDetectKnock = millis();
+    Serial.println("HERE");
+  }
+  if (currentTime - timeSensorDetectKnock >= toneTime && timeSensorDetectKnock != -1) {
+    tone(buzzerPin, toneFrequency);
+  }
+  if (currentTime - timeSensorDetectKnock >= toneTime && buttonValue == HIGH && timeSensorDetectKnock != -1) {
+    noTone(buzzerPin);
+    timeSensorDetectKnock = -1;
+  }
+}
