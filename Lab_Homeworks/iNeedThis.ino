58a578351c02c4893115fd4ea59f23701888e31d
AlexBlagescu <44620859+AlexBlagescu@users.noreply.github.com>
2019-12-17 07:42:15 +0200

    Update iNeedThis.ino

--- Lab_Homeworks/iNeedThis.ino
+++ Lab_Homeworks/iNeedThis.ino
@@ -1,25 +1,85 @@
 #include "LedControl.h"
+#include <LiquidCrystal.h>
+#include <LiquidMenu.h>
 LedControl lc = LedControl(12, 11, 10, 1);
+const byte LCD_RS = 13;
+const byte LCD_E = 6;
+const byte LCD_D4 = 5;
+const byte LCD_D5 = 4;
+const byte LCD_D6 = 3;
+const byte LCD_D7 = 2;
 const int pinSW = 9; // digital pin connected to switch output
 const int pinX = A0; // A0 - analog pin connected to X output
 const int pinY = A1; // A1 - analog pin connected to Y output
 const int maxNumberBullets = 10;
-const int shotDelay = 400;
+const int shotDelay = 200;
 const int monsterDelay = 400;
 const int nrLevel = 4;
 const int monstersPerLevel = 10;
+const int screenUpdateDelay = 60;
+int currentScreen = 0;
+int highScore = 0;
 int lives = 3;
-int gameState = 1;
+int score = 0;
 int currentLevel = 0;
 int countMonsters = 0;
-int timeMonsterCreate = 0;
-int lastSwitchValue = 0;
-int xValue = 0;
-int yValue = 0;
+unsigned long timeMonsterCreate = 0;
+unsigned long timeScreenUpdate = 0;
+int lastSwitchValue = 0, lastSwitchValueMenu = 0;
+int xValue = 0, xValueMenu = 0;
+int yValue = 0, yValueMenu = 0;
 int minThreshold = 400;
 int maxThreshold = 600;
-int switchValue = 0;
-bool joyMoved = false;
+int switchValue = 0, switchValueMenu = 0;
+bool joyMoved = false, joyMovedMenu = false, changeScreen = false;
+
+LiquidCrystal lcd(LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+
+////            WELCOME SCREEN
+LiquidLine welcomeLine1(1, 0, "Welcome to ");
+LiquidLine welcomeLine2(2, 1, "Line Invaders!");
+
+LiquidScreen welcomeScreen(welcomeLine1, welcomeLine2);
+
+////            SECOND SCREEN
+LiquidLine playGameLine(3, 0, "Play Game");
+LiquidLine enterLine(2, 1, "Enter -> JS");
+
+LiquidScreen secondaryScreen(playGameLine, enterLine);
+
+////            THIRD SCREEN
+LiquidLine highScoreLine(3, 0, "High Score");
+
+LiquidScreen thirdScreen(highScoreLine, enterLine);
+
+//              FOURTH SCREEN
+
+LiquidLine settingsLine(3, 0, "Settings");
+
+LiquidScreen fourthScreen(settingsLine, enterLine);
+
+//              FIFTH SCREEN
+
+LiquidLine highScoreDisplayLine(0, 0, "High Score: ", highScore);
+LiquidLine returnLine(0, 1, "Press JS to exit");
+
+LiquidScreen fifthScreen(highScoreDisplayLine, returnLine);
+
+//              SIXTH SCREEN
+
+LiquidLine levelLine(3, 0, "Level: ", currentLevel);
+
+LiquidScreen sixthScreen(levelLine, returnLine);
+
+//              SEVENTH SCREEN
+
+LiquidLine livesLevelLine(0, 0, "Lives: ", lives, " Lvl: ", currentLevel);
+LiquidLine scoreLine(4, 1, "Score: ", score);
+
+LiquidScreen seventhScreen(livesLevelLine, scoreLine);
+
+LiquidMenu menu(lcd);
+
 struct point {
   int x;
   int y;
@@ -30,7 +90,7 @@ struct bullet {
   unsigned long t;
 };
 point shipXY[4] = {
-  {0, 7}, {1, 7}, {2, 7}, {1, 6}
+  {2, 7}, {3, 7}, {4, 7}, {3, 6}
 };
 bullet monsters[4][10][2];
 bullet shotFirstWeapon[maxNumberBullets] = {
@@ -132,10 +192,10 @@ void createMonster() {
   if (millis() - timeMonsterCreate >= 1000 && countMonsters < 10) {
     timeMonsterCreate = millis();
     int randNumber = random(1, 8);
-    monsters[currentLevel][countMonsters][0].y = 1;
+    monsters[currentLevel][countMonsters][0].y = 0;
     monsters[currentLevel][countMonsters][0].x = randNumber - 1;
     monsters[currentLevel][countMonsters][0].t = millis();
-    monsters[currentLevel][countMonsters][1].y = 1;
+    monsters[currentLevel][countMonsters][1].y = 0;
     monsters[currentLevel][countMonsters][1].x = randNumber;
     monsters[currentLevel][countMonsters][1].t = monsters[currentLevel][countMonsters][0].t;
     countMonsters += 1;
@@ -148,9 +208,8 @@ void showMonster() {
         && monsters[currentLevel][j][1].x != -1 && monsters[currentLevel][j][1].t != -1) {
       lc.setLed(0, monsters[currentLevel][j][0].y, monsters[currentLevel][j][0].x, true);
       lc.setLed(0, monsters[currentLevel][j][1].y, monsters[currentLevel][j][1].x, true);
-
     }
-    if (monsters[currentLevel][j][0].y == 7 && monsters[currentLevel][j][1].y == 7) {
+    if (monsters[currentLevel][j][0].y == 8 && monsters[currentLevel][j][1].y == 8) {
       lc.setLed(0, monsters[currentLevel][j][0].y, monsters[currentLevel][j][0].x, false);
       lc.setLed(0, monsters[currentLevel][j][1].y, monsters[currentLevel][j][1].x, false);
       monsters[currentLevel][j][0].y = -1;
@@ -171,6 +230,8 @@ void checkCollision() {
             && monsters[currentLevel][j][1].x != -1 && monsters[currentLevel][j][1].t != -1) {
           if ((shotFirstWeapon[i].y == monsters[currentLevel][j][0].y && shotFirstWeapon[i].x == monsters[currentLevel][j][0].x) ||
               (shotFirstWeapon[i].y == monsters[currentLevel][j][1].y && shotFirstWeapon[i].x == monsters[currentLevel][j][1].x)) {
+            score = score +  max(1.2 * monsters[currentLevel][j][0].y, 1.2 * monsters[currentLevel][j][1].y);
+            menu.update();
             lc.setLed(0, monsters[currentLevel][j][0].y, monsters[currentLevel][j][0].x, false);
             lc.setLed(0, monsters[currentLevel][j][1].y, monsters[currentLevel][j][1].x, false);
             monsters[currentLevel][j][0].y = monsters[currentLevel][j][0].x = monsters[currentLevel][j][0].t = -1;
@@ -211,7 +272,7 @@ bool checkGameOver() {
           (monsters[currentLevel][j][1].y == shipXY[3].y && monsters[currentLevel][j][1].x == shipXY[3].x) ||
           (monsters[currentLevel][j][0].y == shipXY[2].y && monsters[currentLevel][j][0].x == shipXY[2].x) ||
           (monsters[currentLevel][j][1].y == shipXY[0].y && monsters[currentLevel][j][1].x == shipXY[0].x) ||
-          (monsters[currentLevel][j][0].y == 7 && monsters[currentLevel][j][1].y == 7)) {
+          (monsters[currentLevel][j][0].y == 8 && monsters[currentLevel][j][1].y == 8)) {
         lives -= 1;
         monsters[currentLevel][j][0].y = monsters[currentLevel][j][0].x = monsters[currentLevel][j][0].t = -1;
         monsters[currentLevel][j][1].y = monsters[currentLevel][j][0].x = monsters[currentLevel][j][0].t = -1;
@@ -241,26 +302,21 @@ void happyFace() {
   lc.setLed(0, 6, 5, true);
   lc.setLed(0, 5, 6, true);
 }
-void setup() {
-  pinMode(pinSW, INPUT_PULLUP);
-  lc.shutdown(0, false);
-  lc.setIntensity(0, 2);
-  lc.clearDisplay(0);
-  Serial.begin(9600);
-  randomSeed(analogRead(2));
-  for (int i = 0; i < nrLevel; i++)
-    for (int j = 0; j < monstersPerLevel; j++) {
-      monsters[i][j][0].y = -1;
-      monsters[i][j][0].x = -1;
-      monsters[i][j][0].t = -1;
-      monsters[i][j][1].y = -1;
-      monsters[i][j][1].x = -1;
-      monsters[i][j][1].t = -1;
-    }
+bool checkMonstersAlive() {
+  for (int j = 0; j < monstersPerLevel; j++) {
+    if (monsters[currentLevel][j][0].y != -1 && monsters[currentLevel][j][0].x != -1
+        && monsters[currentLevel][j][0].t != -1 && monsters[currentLevel][j][1].y != -1
+        && monsters[currentLevel][j][1].x != -1 && monsters[currentLevel][j][1].t != -1)
+      return true;
+  }
+  return false;
 }
-void loop() {
-  while (gameState) {
-    Serial.println(lives);
+void playGame() {
+  if (changeScreen == false) {
+    changeScreen = true;
+    menu.change_screen(7);
+  }
+  while (1) {
     switchValue = !digitalRead(pinSW);
     xValue = analogRead(pinX);
     yValue = analogRead(pinY);
@@ -275,14 +331,135 @@ void loop() {
     checkCollision();
     if (checkGameOver() == true) {
       sadFace();
-      gameState = !gameState;
-      break;
+      menu.change_screen(2);
+      delay(1000);
+      lc.clearDisplay(0);
+      return;
     }
-    else if (checkGameOver() == false && countMonsters == 10) {
+    else if (checkGameOver() == false && checkMonstersAlive() == false && countMonsters == 10) {
       happyFace();
-      gameState = !gameState;
-      break;
+      menu.change_screen(2);
+      delay(1000);
+      lc.clearDisplay(0);
+      return;
+    }
+    if (millis() - timeScreenUpdate >= 100) {
+      timeScreenUpdate = millis();
+      menu.update();
     }
     lastSwitchValue = switchValue;
   }
+}
+void highScoreFunction() {
+  
+}
+void settingsFunction() {
+  
+}
+void resetGame() {
+  lives = 3;
+  score = 0;
+  countMonsters = 0;
+  timeMonsterCreate = 0;
+  lastSwitchValue = 0;
+  timeScreenUpdate = 0;
+  xValue = 0;
+  yValue = 0;
+  switchValue = 0;
+  joyMoved = false;
+  changeScreen = false;
+  for (int i = 0; i < maxNumberBullets; i++) {
+    shotFirstWeapon[i].y = -1;
+    shotFirstWeapon[i].x = -1;
+    shotFirstWeapon[i].t = -1;
+  }
+  for (int j = 0; j < monstersPerLevel; j++) {
+    monsters[currentLevel][j][0].y = -1;
+    monsters[currentLevel][j][0].x = -1;
+    monsters[currentLevel][j][0].t = -1;
+    monsters[currentLevel][j][1].y = -1;
+    monsters[currentLevel][j][1].x = -1;
+    monsters[currentLevel][j][1].t = -1;
+  }
+  shipXY[0].x = 2;
+  shipXY[0].y = 7;
+  shipXY[1].x = 3;
+  shipXY[1].y = 7;
+  shipXY[2].x = 4;
+  shipXY[2].y = 7;
+  shipXY[3].x = 3;
+  shipXY[3].y = 6;
+}
+void setup() {
+  pinMode(pinSW, INPUT_PULLUP);
+  lc.shutdown(0, false);
+  lc.setIntensity(0, 2);
+  lc.clearDisplay(0);
+  Serial.begin(9600);
+
+  lcd.begin(16, 2);
+  playGameLine.attach_function(0, playGame);
+  highScoreLine.attach_function(1, highScoreFunction);
+  settingsLine.attach_function(2, settingsFunction);
+  menu.add_screen(welcomeScreen);
+  menu.add_screen(secondaryScreen);
+  menu.add_screen(thirdScreen);
+  menu.add_screen(fourthScreen);
+  menu.add_screen(fifthScreen);
+  menu.add_screen(sixthScreen);
+  menu.add_screen(seventhScreen);
+  menu.update();
+
+  randomSeed(analogRead(2));
+  for (int i = 0; i < nrLevel; i++)
+    for (int j = 0; j < monstersPerLevel; j++) {
+      monsters[i][j][0].y = -1;
+      monsters[i][j][0].x = -1;
+      monsters[i][j][0].t = -1;
+      monsters[i][j][1].y = -1;
+      monsters[i][j][1].x = -1;
+      monsters[i][j][1].t = -1;
+    }
+}
+void loop() {
+  if (currentScreen == 0) {
+    delay(2000);
+    menu.next_screen();
+    menu.set_focusedLine(0);
+    currentScreen += 1;
+  }
+  switchValueMenu = !digitalRead(pinSW);
+  xValueMenu = analogRead(pinX);
+  yValueMenu = analogRead(pinY);
+  if (yValueMenu < minThreshold && joyMovedMenu == false) {
+    joyMovedMenu = true;
+    currentScreen -= 1;
+    if (currentScreen < 1) {
+      currentScreen = 1;
+    }
+    else {
+      menu.previous_screen();
+      menu.set_focusedLine(0);
+    }
+  }
+  if (yValueMenu > maxThreshold && joyMovedMenu == false) {
+    joyMovedMenu = true;
+    currentScreen += 1;
+    if (currentScreen > 3) {
+      currentScreen = 3;
+    }
+    else {
+      menu.next_screen();
+      menu.set_focusedLine(0);
+    }
+  }
+  if (yValueMenu >= minThreshold && yValueMenu <= maxThreshold) {
+    joyMovedMenu = false;
+  }
+  if (switchValueMenu == 1 && lastSwitchValueMenu == 0 && currentScreen == 1) {
+    delay(500);
+    menu.call_function(0);
+    resetGame();
+  }
+  lastSwitchValueMenu = switchValueMenu;
 }
